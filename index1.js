e44449015ccf1ab01b9c5de7a79468c699e933f3
devlrakib <devrakib5@gmail.com>
2024-01-14 01:55:14 +0600

    first commit

---
+++ index1.js
@@ -0,0 +1,16 @@
+// For Loop Break Formula
+for(var i=1; i<=100; i++){
+    if(i==10){
+        break;
+    }
+    document.write(" "+i+"<br>");
+    
+}
+
+// Lop Conditional Continue Formula
+for(var i=101; i<=200; i++){
+    if(i==110){
+        continue; // Aikhane Contine Use Korar Jonno 110 Ta print kore ni 110 er Por Theke Print Korbe
+    }
+    document.write(" "+i);
+}
